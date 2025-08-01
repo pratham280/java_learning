3e7ac90c46a0cad64ba8a0ed281e2a75eb0d8b24
Prathamesh Mawale <prathameshmawale@gmail.com>
2025-08-01 12:48:04 +0530

    intro to java covered the sum of two integers and then got to know about typecasting

---
+++ Intro to Java/TypeCasting.java
@@ -0,0 +1,15 @@
+
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        // Implicit casting (widening)
+        int num = 100;
+        double d = num;  // int to double
+        System.out.println("Implicit casting (int to double): " + d);
+
+        // Explicit casting (narrowing)
+        double pi = 3.14;
+        int intPi = (int) pi;  // double to int
+        System.out.println("Explicit casting (double to int): " + intPi);
+    }
+}
